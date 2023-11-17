1bf4adca9309c2c3d0c480f788ed6308e49c338d
Jesus Ambro <68930496+jesusxambro@users.noreply.github.com>
2023-11-16 20:16:38 -0600

    finished 24

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [goalInput, setGoalInput] = useState("");
+  const [listOfGoals, setListOfGoals] = useState<string[]>([]);
+  function goalInputHandler(enteredText: string){
+    setGoalInput(enteredText)
+  };
+  function addGoalHandler(){
+    const goalToSave = goalInput;
+    setGoalInput("");
+    setListOfGoals(currentGoals => [...currentGoals, goalToSave]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} 
+        placeholder="Your course goal!"
+        onChangeText={goalInputHandler}
+        />
+        <Button onPress={addGoalHandler} title="Add Goal"/>
+      </View>
+      <View style={styles.goalsContainer}>
+        {listOfGoals.map((goal, index)=>{
+          return(
+            <View style={styles.listGoals} key={index}>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          )
+        })}
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer:{
+    flex:1,
+    padding:50,
+    paddingHorizontal:16,
   },
+  inputContainer:{
+    flex: 1,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    paddingBottom:24,
+    borderBottomWidth:1,
+    borderBottomColor:'#cccc',
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor:'#cccc',
+    width:'70%',
+    marginRight:8,
+    padding:8,
+  },
+  goalsContainer:{
+    flex:5,
+  },
+  listGoals:{
+    margin:8,
+    borderRadius:6,
+    padding:8,
+    color:'white',
+    borderColor:'black',
+    backgroundColor:'#5e0acc'
+  },
+  goalText:{
+    color:'white',
+  }
 });
