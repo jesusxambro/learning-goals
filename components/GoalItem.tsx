f4d2dd93da33a1e6e908133f90ab7c60e0bd3023
jesusxambro <jesus.ambrocio@outlook.com>
2023-11-19 18:27:30 -0600

    added animations to presses

--- components/GoalItem.tsx
+++ components/GoalItem.tsx
@@ -16,26 +16,27 @@ interface goalItemInterface {
 
 function GoalItem({ item, deleteGoal }: goalItemInterface) {
   return (
-    <Pressable onPress={() => {
-      Alert.alert(`Goal: ${item.text}`, 'Goal Details..', [
-        {
-          text: 'Delete', 
-          // found a big bug, it deletes the wrong one... 
-          onPress: () => deleteGoal(item.index),
-          style:'destructive'
-        },
-        {
-          text: 'Cancel',
-          onPress: () => console.log('Cancel Pressed'),
-          style: 'cancel',
-        },
-        {text: 'OK', onPress: () => console.log('OK Pressed')},
-      ]);
-    }} >
-      <View style={goalItemStyles.listGoals}>
+    <View style={goalItemStyles.listGoals}>
+        <Pressable android_ripple={{color:"#dddddd"}}
+         style={({pressed}) => pressed && goalItemStyles.pressedItem}
+        onPress={() => {
+          Alert.alert(`Goal: ${item.text}`, 'Goal Details..', [
+            {
+              text: 'Delete', 
+              onPress: () => deleteGoal(item.index),
+              style:'destructive'
+            },
+            {
+              text: 'Cancel',
+              onPress: () => console.log('Cancel Pressed'),
+              style: 'cancel',
+            },
+            {text: 'OK', onPress: () => console.log('OK Pressed')},
+          ]);
+        }} >
         <Text style={goalItemStyles.goalText}>{item.text}</Text>
-      </View>
     </Pressable>
+      </View>
   );
 }
 
@@ -45,12 +46,16 @@ const goalItemStyles = StyleSheet.create({
   listGoals: {
     margin: 8,
     borderRadius: 6,
-    padding: 8,
     color: "white",
     borderColor: "black",
     backgroundColor: "#5e0acc",
   },
+  pressedItem:{
+    opacity:0.5
+  },
+
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
